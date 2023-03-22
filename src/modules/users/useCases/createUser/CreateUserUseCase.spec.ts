e07ea013d779a38cc3e480622ea2e6e6c56df034
Thiago Nascimento <thiago.nascimento8512@gmail.com>
2023-03-21 21:53:45 -0300

    Finished create user tests

--- src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -1,16 +1,20 @@
 import { User } from "../../entities/User";
 import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { CreateUserError } from "./CreateUserError";
+import { CreateUserUseCase } from "./CreateUserUseCase";
 
 let usersRepository: IUsersRepository;
+let createUserUseCase: CreateUserUseCase;
 
 describe("Create User", () => {
   beforeEach(() => {
     usersRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(usersRepository);
   });
 
   it("should be able to create a new user", async () => {
-    const user = await usersRepository.create({
+    const user = await createUserUseCase.execute({
       name: "User Test",
       email: "user@email.com",
       password: "1234",
@@ -18,5 +22,21 @@ describe("Create User", () => {
 
     expect(user).toBeInstanceOf(User);
     expect(user).toHaveProperty("id");
+  });
+
+  it("should not be able to create a new user with an email that already exists", async () => {
+    expect(async () => {
+      await createUserUseCase.execute({
+        name: "User Test",
+        email: "user@email.com",
+        password: "1234",
+      });
+
+      await createUserUseCase.execute({
+        name: "User Test",
+        email: "user@email.com",
+        password: "1234",
+      });
+    }).rejects.toBeInstanceOf(CreateUserError);
   });
 });
