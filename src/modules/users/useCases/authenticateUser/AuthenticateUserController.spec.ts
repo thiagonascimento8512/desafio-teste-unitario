7fce3f9296b7f19351b411d61ec54e4f9cc20ea5
Thiago Nascimento <thiago.nascimento8512@gmail.com>
2023-03-24 22:53:54 -0300

    Created file intergration test for authenticate user

---
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserController.spec.ts
@@ -0,0 +1,10 @@
+
+describe("Authenticate User Controller", () => {
+  it("should be able to authenticate an user", async () => {
+    const user = {
+      name: "User Example",
+      email: "user@memail.com",
+      password: "1234",
+    }
+  });
+});
